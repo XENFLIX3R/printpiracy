3850025fbf7aac168fe0d858eff5d191063bd65c
YO_DI3G0 <diegocda1212@gmail.com>
2025-05-15 20:48:09 -0500

    Create content.jsx

---
+++ src/config/content.jsx
@@ -0,0 +1,81 @@
+export const games = [
+      {
+        name: "Skull Island Skirmish",
+        embedCode: "<p>Embed code for Skull Island Skirmish</p>",
+        imagePlaceholder: "A pirate ship battling a kraken near a skull-shaped island, black and white sketch style",
+        description: "Defend your treasure in this thrilling tower defense game.",
+      },
+      {
+        name: "Cannonball Chaos",
+        embedCode: "<p>Embed code for Cannonball Chaos</p>",
+        imagePlaceholder: "Exploding cannonballs on a ship deck, dynamic black and white action shot",
+        description: "Aim true and sink the enemy fleet before they sink you!",
+      },
+      {
+        name: "Treasure Hunt Tycoon",
+        embedCode: "<p>Embed code for Treasure Hunt Tycoon</p>",
+        imagePlaceholder: "A treasure map with an X marking the spot, old parchment style, monochrome",
+        description: "Follow the clues, dig for gold, and become the richest pirate.",
+      },
+      {
+        name: "Sea Serpent's Maze",
+        embedCode: "<p>Embed code for Sea Serpent's Maze</p>",
+        imagePlaceholder: "A menacing sea serpent in a dark, stormy sea, detailed black and white illustration",
+        description: "Navigate treacherous waters and escape the beast.",
+      },
+    ];
+
+    export const apps = [
+      {
+        name: "PiratePad Notes",
+        embedCode: "<p>Embed code for PiratePad Notes</p>",
+        imagePlaceholder: "A quill pen writing on a scroll, black and white, simple icon",
+        description: "Jot down your secret plans and treasure locations.",
+      },
+      {
+        name: "Captain's Compass",
+        embedCode: "<p>Embed code for Captain's Compass</p>",
+        imagePlaceholder: "An ornate antique compass, monochrome, detailed",
+        description: "Never lose your way with this trusty navigation tool.",
+      },
+      {
+        name: "Jolly Roger Chat",
+        embedCode: "<p>Embed code for Jolly Roger Chat</p>",
+        imagePlaceholder: "Two crossed swords over a skull, classic pirate flag design, white on black",
+        description: "Communicate with your crew securely (not really).",
+      },
+      {
+        name: "Lookout Weather",
+        embedCode: "<p>Embed code for Lookout Weather</p>",
+        imagePlaceholder: "Stormy clouds and lightning over a dark sea, dramatic black and white",
+        description: "Check the forecast before setting sail.",
+      },
+    ];
+
+    export const cloakOptions = [
+      {
+        name: "Default (Piracy)",
+        title: "Piracy - Navigate Freely",
+        iconUrl: "/vite.svg", 
+      },
+      {
+        name: "Google Drive",
+        title: "Google Drive",
+        iconUrl: "https://ssl.gstatic.com/images/branding/product/1x/drive_2020q4_32dp.png",
+      },
+      {
+        name: "Google Classroom",
+        title: "Google Classroom",
+        iconUrl: "https://ssl.gstatic.com/classroom/favicon.png",
+      },
+      {
+        name: "New Tab",
+        title: "New Tab",
+        iconUrl: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAQAAADZc7J/AAAAKklEQVR42mNgGAWjYBSMAqoCBBgAacgA9IAwSAfSg4GgNsgADgMAHAbn9f1pvjgAAAAASUVORK5CYII=", // Simple blank icon
+      },
+      {
+        name: "Custom Image",
+        title: "My Awesome Page",
+        iconUrl: "https://image.com",
+      }
+    ];
